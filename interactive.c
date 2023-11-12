923db49063c3e88be1de0401e8059dd7546b8ee8
Y-admen <yassminasg305@gmail.com>
2023-11-12 04:10:18 +0200

    s_shell

---
+++ interactive.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+void interactiv(char *list_arg)
+{
+	char *prompt = "$ ", **arr = NULL, *buff = NULL, *lineptr = NULL;
+	size_t buf_size = 0;
+	int  i = 1, status = 0;
+
+	while(1)
+	{
+		_putchar(prompt)
+	}
+}
---
+++ remove_comment.c
@@ -0,0 +1,20 @@
+#include"main.h"
+
+void remove_comment(char *str)
+{
+	int i = 0;
+
+	while ((str)[i] != '\0')
+	{
+		if ((str)[i] == '#')
+		{
+			if (i == 0 || (str)[i - 1] == ' ')
+			{
+				(str)[i] = '\0';
+				break;
+			}
+		}
+		i++;
+	}
+	return(str);
+}
